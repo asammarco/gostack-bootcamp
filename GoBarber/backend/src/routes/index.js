0d24188ad8cf1c698b6896f2fd108e03c1df6525
asammarco <asammarco@hotmail.com>
2020-02-03 09:44:16 -0300

    add nodemon

--- GoBarber/backend/src/routes/index.js
+++ GoBarber/backend/src/routes/index.js
@@ -1,3 +1,8 @@
-module.exports = {
-  
-}
+const { Router } = require('express');
+const routes = Router();
+
+routes.get('/', (req,res) => {
+  res.send('Hello World!');
+})
+
+module.exports = routes;
